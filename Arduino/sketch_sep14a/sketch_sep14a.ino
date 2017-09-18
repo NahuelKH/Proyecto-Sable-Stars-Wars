74a60d8b5dc4033a464f12a9a54ea7db0d736d3e
hymuura <daniel.espinola.hym@gmail.com>
2017-09-17 22:19:22 -0300

    prueba de commit

--- Arduino/sketch_sep14a/sketch_sep14a.ino
+++ Arduino/sketch_sep14a/sketch_sep14a.ino
@@ -2,10 +2,14 @@
 #include <I2Cdev.h>
 #include <MPU6050.h>
 
+#define DEBUGG_MODE 0
 
 MPU6050 mpu;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
+double normal;
+double normal2;
+int contadorDeTurno=0;
 
 //Constantes para pines de sensores
 #define PIN_SENSOR_LDR_ANALOGICO A3
@@ -25,6 +29,17 @@ int16_t gx, gy, gz;
 #define COMMON_ANODE
 
 #define DEFAULT_COLOR 0
+
+//umbrales
+#define UMBRAL_ACELEROMETRO 3000
+#define UMBRAL_LUZ_AMBIENTAL 100
+#define UMBRAL_LUZ_BRILLO 120
+#define UMBRAL_LUZ_OSCURIDAD 90
+//contadores de sonido
+int attack = 0;
+int nonAttack = 0;
+int semAttack = 0;
+int luz=0;
 /**
   Método que inicializa/configura el arduino.
 */
@@ -38,7 +53,7 @@ void setup() {
   pinMode(PIN_SDA_6050_ANALOGICO, INPUT);
   Serial.println(mpu.testConnection() ? "Connected" : "Connection failed");
   // Seteo de pines DIGITALES
- // pinMode(PIN_REED_DIGITAL, INPUT);
+  pinMode(PIN_REED_DIGITAL, INPUT);
 /*  
   pinMode(PIN_SCK_TARJETA_SD_DIGITAL, OUTPUT);
   pinMode(PIN_MISO_TARJETA_SD_DIGITAL, OUTPUT);
@@ -49,58 +64,91 @@ void setup() {
 
   setColor(DEFAULT_COLOR, DEFAULT_COLOR, DEFAULT_COLOR);
   */
+  
+  //configuracion mpu
+  mpu.setXAccelOffset(-1577);
+  mpu.setYAccelOffset(61);
+  mpu.setZAccelOffset(1171);
+  mpu.setXGyroOffset(-70);
+  mpu.setYGyroOffset(36);
+  mpu.setZGyroOffset(-16);
+  //fin
+
+  Serial.print("ready");
 }
 
 /**
   Método en el que se programa la funcionalidad
 */
 void loop() {
-
-    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-   // ax+=-1376;
-   Serial.print(ax);
-   Serial.print(" ");
-   Serial.print(ay);
-   Serial.print(" ");
-   Serial.print(az);
-   Serial.print(" ");
-
-   Serial.print(gx);
-   Serial.print(" ");
-   Serial.print(gy);
-   Serial.print(" ");
-   Serial.print(gz);
-   Serial.println();
-  
-  
-
-  //Serial.println();
-   delay(2000);
- /* if(reed){
-    // Luz
-    intensidad = sensarLuzExterna();
-    ajustarIntensidad();
-
-
-    // 
-    
-  }
-  
-  if (hayMovimiento()) {
-    sonarSableMovimiento();
-  }
-
-  setColor(0, 0, 255);  // Azul
-*/
+	Serial.println("leyendo");
+	sensarMovimiento();
+  sensarLuz();
+	delay(2000);
 }
 
+void sensarLuz(){
+    luz=analogRead(PIN_SENSOR_LDR_ANALOGICO);
+    Serial.print("Luz: ");
+    Serial.println(luz);
+}
  
+void sensarMovimiento(){
+	mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+	if(DEBUGG_MODE){
+		Serial.print(ax);
+		Serial.print(" ");
+		Serial.print(ay);
+		Serial.print(" ");
+		Serial.print(az);
+		Serial.print(" ");
+
+		Serial.print(gx);
+		Serial.print(" ");
+		Serial.print(gy);
+		Serial.print(" ");
+		Serial.print(gz);
+		Serial.println();
+	}
+	
+	//calculo la normal de las aceleraciones
+	if(contadorDeTurno%2==0){
+		normal=sqrt(pow(ax,2)+pow(ay,2)+pow(az,2));
+	}else{
+		normal2=sqrt(pow(ax,2)+pow(ay,2)+pow(az,2));  
+	}
+	contadorDeTurno++;
+	//reseteo el contador para que no se vaya a la mierda
+	if(contadorDeTurno==99){
+		contadorDeTurno=0;
+    }
+	
+	//para mi hay que usar una logica de contadores(en tiempo) como dijo el profe
+	if(hayMovimiento(abs(normal-normal2))){
+		//reproducir sonido
+   Serial.println("hay movimiento");
+		attack++;
+		sonarSable();
+	}else{
+		//preparar logica para el neutro
+    Serial.println("*******************************************************neutro");
+		nonAttack++;
+		sonarSable();
+	}
+} 
+
 /*
    método que indicará si el sable de luz produjo un movimiento.
    @author: Pablo
 */
-boolean hayMovimiento() {
-  return true;
+//detecta movimiento segun el umbral
+bool hayMovimiento(double movimiento) {
+  bool flagMovimiento = 0;
+  if(movimiento>UMBRAL_ACELEROMETRO){
+    flagMovimiento = 1;
+    normal=0;
+  }
+  return flagMovimiento;
 }
 
 /*
@@ -108,7 +156,17 @@ boolean hayMovimiento() {
    @author: Pablo
 */
 void sonarSable() {
-
+	if(attack==1){
+		//reproducir sonido 1
+	}else if(attack==2){
+		//reproducir sonido 2
+	}else if(attack==3){
+		//reproducir combo
+		//ATTACK=0;
+	}
+	if(nonAttack==1){
+		//reproducir sonido neutro
+	}
 }
 
 /*
